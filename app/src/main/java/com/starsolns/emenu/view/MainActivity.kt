260b71ae0dd77372828d09891e71290fe53cbced
jmngige <jamesmngige@gmail.com>
2022-03-15 10:23:32 +0300

    Initial Commit

---
+++ app/src/main/java/com/starsolns/emenu/view/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.starsolns.emenu.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.starsolns.emenu.R
+import com.starsolns.emenu.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfig: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHost) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        appBarConfig = AppBarConfiguration(
+            setOf(R.id.homeFragment, R.id.dashboardFragment, R.id.notificationsFragment)
+        )
+
+        binding.bottomNav.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfig)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+}
