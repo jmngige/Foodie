daecd83de8621003d99be4b0807f653bb4684c66
jmngige <jamesmngige@gmail.com>
2022-03-16 09:30:05 +0300

    Permissions Commit

--- app/src/main/java/com/starsolns/emenu/view/AddMealActivity.kt
+++ app/src/main/java/com/starsolns/emenu/view/AddMealActivity.kt
@@ -1,17 +1,28 @@
 package com.starsolns.emenu.view
 
+import android.Manifest
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.ViewGroup
 import android.widget.Toast
-import com.starsolns.emenu.R
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.starsolns.emenu.databinding.ActivityAddMealBinding
-import com.starsolns.emenu.databinding.ActivityMainBinding
 import com.starsolns.emenu.databinding.CustomAddImageLayoutBinding
 
 class AddMealActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddMealBinding
+    private val dialog = Dialog(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddMealBinding.inflate(layoutInflater)
@@ -26,15 +37,17 @@ class AddMealActivity : AppCompatActivity() {
     }
 
     private fun loadCustomAddImageOptions() {
-        val customBinding  = CustomAddImageLayoutBinding.inflate(layoutInflater)
-        val dialog = Dialog(this)
+        val customBinding = CustomAddImageLayoutBinding.inflate(layoutInflater)
         dialog.setContentView(customBinding.root)
-        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
         dialog.show()
 
         customBinding.galleryOption.setOnClickListener {
-            Toast.makeText(this, "Choose from phone gallery", Toast.LENGTH_LONG).show()
-            dialog.dismiss()
+            requestGalleryAccessPermissions()
+
         }
 
         customBinding.cameraOption.setOnClickListener {
@@ -42,6 +55,47 @@ class AddMealActivity : AppCompatActivity() {
             dialog.dismiss()
         }
 
+    }
+
+    private fun requestGalleryAccessPermissions() {
+        Dexter.withContext(this).withPermissions(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ).withListener(object : MultiplePermissionsListener {
+            override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                if (report!!.areAllPermissionsGranted()) {
+                    Toast.makeText(applicationContext, "Gallery Option Accepted ", Toast.LENGTH_LONG).show()
+                    dialog.dismiss()
+                }else{
+                    showCustomDialogOnPermissionsDeny()
+                }
+            }
+            override fun onPermissionRationaleShouldBeShown(
+                permsRequest: MutableList<PermissionRequest>?,
+                token: PermissionToken?
+            ) {
+                showCustomDialogOnPermissionsDeny()
+            }
+        }).onSameThread().check()
+    }
+
+    private fun showCustomDialogOnPermissionsDeny() {
+        AlertDialog.Builder(this)
+            .setMessage("Some features won't work correctly without enabling these permissions")
+            .setTitle("!Warning")
+            .setPositiveButton("Settings"){_,_->
+                try{
+                    val settingsIntent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package",packageName,null)
+                    settingsIntent.data = uri
+                    startActivity(settingsIntent)
+                }catch (e : ActivityNotFoundException){
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel"){_,_->
+                dialog.dismiss()
+            }.show()
     }
 
 
