8954f36f890b2d45defbc5444c4d6cbbd77dfc9a
jmngige <jamesmngige@gmail.com>
2022-03-16 20:42:48 +0300

    Image selection Commit

--- app/src/main/java/com/starsolns/emenu/view/AddMealActivity.kt
+++ app/src/main/java/com/starsolns/emenu/view/AddMealActivity.kt
@@ -4,17 +4,28 @@ import android.Manifest
 import android.app.Activity
 import android.app.Dialog
 import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.ContextWrapper
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.provider.Settings
+import android.util.Log
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
@@ -23,10 +34,16 @@ import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import com.starsolns.emenu.R
 import com.starsolns.emenu.databinding.ActivityAddMealBinding
 import com.starsolns.emenu.databinding.CustomAddImageLayoutBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.util.*
 
 class AddMealActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddMealBinding
 
+    private var imagePath: String = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -126,20 +143,78 @@ class AddMealActivity : AppCompatActivity() {
             }.show()
     }
 
+    //save images captured and images selected to phones internal storage
+    private fun saveSelectedImagesToInternalStorage(bitmap: Bitmap): String{
+        val wrapper = ContextWrapper(applicationContext)
+
+        var file = wrapper.getDir(EMENU_IMAGE_DIRECTORY, Context.MODE_PRIVATE)
+        file = File(file, "emenu.${UUID.randomUUID()}.jpg")
+
+        try{
+            val stream: OutputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream)
+            stream.flush()
+            stream.close()
+        }catch (e: IOException){
+            e.printStackTrace()
+        }
+
+        return file.absolutePath
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if(resultCode == Activity.RESULT_OK){
             if(requestCode == CAMERA_OPTION_REQUEST_CODE){
                 data?.let {
                     val capturedImage = data.extras!!.get("data") as Bitmap
-                    binding.addMealImageView.setImageBitmap(capturedImage)
+
+                    Glide.with(this)
+                        .load(capturedImage)
+                        .centerCrop()
+                        .into(binding.addMealImageView)
+
+                    imagePath = saveSelectedImagesToInternalStorage(capturedImage)
+                    Log.i(TAG, imagePath)
                     binding.addMealImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_edit))
                 }
             }
             if(requestCode == GALLERY_OPTION_REQUEST_CODE){
                 data?.let {
-                    val image = data.data
-                    binding.addMealImageView.setImageURI(image)
+                    val selectedImage = data.data
+                    Glide.with(this)
+                        .load(selectedImage)
+                        .centerCrop()
+                        .diskCacheStrategy(DiskCacheStrategy.ALL)
+                        .listener(object: RequestListener<Drawable>{
+                            override fun onLoadFailed(
+                                e: GlideException?,
+                                model: Any?,
+                                target: Target<Drawable>?,
+                                isFirstResource: Boolean
+                            ): Boolean {
+                                return false
+                            }
+
+                            override fun onResourceReady(
+                                resource: Drawable?,
+                                model: Any?,
+                                target: Target<Drawable>?,
+                                dataSource: DataSource?,
+                                isFirstResource: Boolean
+                            ): Boolean {
+                                resource?.let {
+
+                                    val bitmap: Bitmap = resource.toBitmap()
+                                    imagePath = saveSelectedImagesToInternalStorage(bitmap)
+                                    Log.i(TAG, imagePath)
+                                }
+                                return false
+                            }
+
+                        })
+                        .into(binding.addMealImageView)
+
                     binding.addMealImage.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_edit))
                 }
             }
@@ -147,8 +222,11 @@ class AddMealActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val GALLERY_OPTION_REQUEST_CODE = 100
-        const val CAMERA_OPTION_REQUEST_CODE = 101
+        private const val GALLERY_OPTION_REQUEST_CODE = 100
+        private const val CAMERA_OPTION_REQUEST_CODE = 101
+
+        private const val EMENU_IMAGE_DIRECTORY = "eMenuImages"
+        private const val  TAG = "ImagePath"
     }
 
 }
