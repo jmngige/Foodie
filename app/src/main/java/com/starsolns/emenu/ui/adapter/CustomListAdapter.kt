79054220367816f7edfa1ad64dbecb125612bbb3
jmngige <jamesmngige@gmail.com>
2022-03-17 11:09:27 +0300

    Initial commit

--- app/src/main/java/com/starsolns/emenu/ui/adapter/CustomListAdapter.kt
+++ app/src/main/java/com/starsolns/emenu/ui/adapter/CustomListAdapter.kt
@@ -1,4 +1,32 @@
 package com.starsolns.emenu.ui.adapter
 
-class CustomListAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.starsolns.emenu.databinding.CustomItemLayoutBinding
+
+class CustomListAdapter(
+    private val context: Context,
+    private val itemsList: List<String>,
+    private val selection: String
+): RecyclerView.Adapter<CustomListAdapter.ViewHolder>(){
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding = CustomItemLayoutBinding.inflate(LayoutInflater.from(context), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val currentItem = itemsList[position]
+        holder.listItem.text = currentItem
+    }
+
+    override fun getItemCount() = itemsList.size
+
+    class ViewHolder(binding: CustomItemLayoutBinding): RecyclerView.ViewHolder(binding.root){
+        val listItem = binding.mealItemSelect
+    }
+
 }
--- app/src/main/java/com/starsolns/emenu/util/Constants.kt
+++ app/src/main/java/com/starsolns/emenu/util/Constants.kt
@@ -2,19 +2,72 @@ package com.starsolns.emenu.util
 
 object Constants {
 
-//    val MEAL_TYPE = "meaType"
-//    val MEAL_CATEGORY = "mealCategory"
-//    val MEAL_DURATION = "mealDuration"
-//
-//
-//    fun getMealTypes(): ArrayList<String>{
-//        val typesList = ArrayList<String>()
-//
-//        typesList.add("Breakfast")
-//
-//
-//
-//        return typesList
-//    }
+    const val MEAL_TYPE: String = "meaType"
+    const val MEAL_CATEGORY: String = "mealCategory"
+    const val MEAL_DURATION: String = "mealDuration"
+
+
+    fun getMealTypes(): ArrayList<String>{
+        val typesList = ArrayList<String>()
+
+        typesList.add("Breakfast")
+        typesList.add("Brunch")
+        typesList.add("Lunch")
+        typesList.add("Snacks")
+        typesList.add("Supper")
+        typesList.add("Salad")
+        typesList.add("Desert")
+        typesList.add("Other")
+
+        return typesList
+    }
+
+    fun getMealCategories(): ArrayList<String>{
+        val categoriesList = ArrayList<String>()
+
+        categoriesList.add("Pizza")
+        categoriesList.add("Chicken")
+        categoriesList.add("Salad")
+        categoriesList.add("Burger")
+        categoriesList.add("Juice")
+        categoriesList.add("Smokies")
+        categoriesList.add("Sandwich")
+        categoriesList.add("Coffee")
+        categoriesList.add("Tea")
+        categoriesList.add("Cake")
+        categoriesList.add("BBQ")
+        categoriesList.add("Chapati")
+        categoriesList.add("Beef")
+        categoriesList.add("Ugali")
+        categoriesList.add("Other")
+
+        return categoriesList
+    }
+
+    fun getMealDuration(): ArrayList<String>{
+        val durationList = ArrayList<String>()
+
+        durationList.add("5-10")
+        durationList.add("10-15")
+        durationList.add("15-20")
+        durationList.add("20-25")
+        durationList.add("25-30")
+        durationList.add("30-35")
+        durationList.add("35-40")
+        durationList.add("40-45")
+        durationList.add("45-50")
+        durationList.add("50-60")
+        durationList.add("60-65")
+        durationList.add("65-70")
+        durationList.add("70-75")
+        durationList.add("75-80")
+        durationList.add("80-85")
+        durationList.add("85-90")
+        durationList.add("90-100")
+
+        return durationList
+    }
+
+
 
 }
